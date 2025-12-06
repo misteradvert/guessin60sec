78856298bb990551a72bba27e113bd785e760bb6
mr.advert <mr.advert@yandex.ru>
2025-12-06 17:07:43 +0300

    CardPlayPage added

---
+++ src/components/pages/CardPlayPage.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import TeamBtn from "../TeamBtn/TeamBtn";
+
+function CardPlayPage() {
+  let allCardsArr = {
+    card_1: [
+      "Безумный Макс",
+      "Игра Престолов",
+      "Depeche Mode",
+      "Бред Пит",
+      "Elton John",
+      "Симба",
+      "Лютер",
+      "Мулан",
+    ],
+    card_2: [
+      "Большой добрый великан",
+      "Ривердейл",
+      "Evanescence",
+      "Риз Уизерспун",
+      "Сплин",
+      "Джесси Пинкман",
+      "Сорвиголова",
+      "Бойцовский клуб",
+    ],
+    card_3: [
+      "Диктатор",
+      "Монстр",
+      "Radiohead",
+      "Натали Портман",
+      "Slipknot",
+      "Торин Дубощит",
+      "И снова здравствуйте",
+      "Таинственный сад",
+    ],
+  };
+
+  return <></>;
+}
+
+export default CardPlayPage;
