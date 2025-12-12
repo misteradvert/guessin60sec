9e93cbb0152eb0dc512358f049f59db73db92ffd
mr.advert <mr.advert@yandex.ru>
2025-12-13 00:47:20 +0300

    HomePage styles and background.svg added

--- src/components/OneCard.jsx
+++ src/components/OneCard.jsx
@@ -29,7 +29,7 @@ function OneCard() {
           })}
         </CardContent>
         <CardActions>
-          <Button size="small">Learn More</Button>
+          <Button size="small">След.карта</Button>
         </CardActions>
       </Card>
     </div>
@@ -37,23 +37,3 @@ function OneCard() {
 }
 
 export default OneCard;
-
-/* <Card sx={{ minWidth: 275 }}>
-  <CardContent>
-    <Typography gutterBottom sx={{ color: "text.secondary", fontSize: 14 }}>
-      Word of the Day
-    </Typography>
-    <Typography variant="h5" component="div">
-      benevolent
-    </Typography>
-    <Typography sx={{ color: "text.secondary", mb: 1.5 }}>adjective</Typography>
-    <Typography variant="body2">
-      well meaning and kindly.
-      <br />
-      {'"a benevolent smile"'}
-    </Typography>
-  </CardContent>
-  <CardActions>
-    <Button size="small">Learn More</Button>
-  </CardActions>
-</Card>; */
--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import StartBtn from "../StartBtn/StartBtn";
+import "../pages/HomePage.css";
 
 function HomePage() {
   const navigate = useNavigate();
@@ -11,8 +12,12 @@ function HomePage() {
 
   return (
     <>
-      <div className="mainHeader">Угадать за 60 секунд</div>
-      <StartBtn name="Начать игру" onClick={handleStartGame} />
+      <div class="divMain">
+        <div class="divAllContent">
+          <div className="mainHeader">Угадать за 60 секунд</div>
+          <StartBtn name="Начать игру" onClick={handleStartGame} />
+        </div>
+      </div>
     </>
   );
 }
