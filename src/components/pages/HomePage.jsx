d9ace5bfcf516ff158802aae05d33137280f5cbc
mr.advert <mr.advert@yandex.ru>
2025-12-20 18:41:24 +0300

    homePage styles added

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -14,9 +14,10 @@ function HomePage() {
     <>
       <div className="divMain">
         <div className="divAllContent">
-          <div className="divLogoBtn">
-            <img className="mainLogo" src="/logotype.png" alt="Лого Угадай за 60 секунд" style={{ border: "1px solid blue" }}></img>
-            <StartBtn className="startBtn" name="Начать игру" onClick={handleStartGame} />
+          <div className="divLogoBtn" style={{ height: "100%", border: "2px solid lime", dispay: "flex", flexDirection: "column", justifyContent: "flex-start", alignItems: "center" }}>
+            <img className="mainLogo" src="/logotype.png" alt="Лого Угадай за 60 секунд" style={{ width: "80%", border: "1px solid blue", marginTop: "10vh" }}></img>
+            <div style={{ textAlign: "center", border: "1px solid black", width: "80%", fontSize: "25px", marginTop: "10%" }}>Объясни максимальное количество слов своей команде за 60 секунд</div>
+            <StartBtn className="startBtn" name="Начать игру" onClick={handleStartGame} sx={{ marginTop: "30%" }} />
           </div>
         </div>
       </div>
