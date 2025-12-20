f41d1a717e3204d38c0ac301e0abd8a77142cd83
mr.advert <mr.advert@yandex.ru>
2025-12-20 19:48:57 +0300

    new font added + TeamChoosePage styles changed

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -16,7 +16,7 @@ function HomePage() {
         <div className="divAllContent">
           <div className="divLogoBtn" style={{ height: "100%", border: "2px solid lime", dispay: "flex", flexDirection: "column", justifyContent: "flex-start", alignItems: "center" }}>
             <img className="mainLogo" src="/logotype.png" alt="Лого Угадай за 60 секунд" style={{ width: "80%", border: "1px solid blue", marginTop: "10vh" }}></img>
-            <div style={{ textAlign: "center", border: "1px solid black", width: "80%", fontSize: "25px", marginTop: "10%" }}>Объясни максимальное количество слов своей команде за 60 секунд</div>
+            <div style={{ textAlign: "center", border: "1px solid black", width: "80%", fontSize: "35px", marginTop: "10%", fontFamily: "'BabyPop', sans-serif", fontWeight: "500" }}>Объясни максимальное количество слов своей команде за 60 секунд</div>
             <StartBtn className="startBtn" name="Начать игру" onClick={handleStartGame} sx={{ marginTop: "30%" }} />
           </div>
         </div>
--- src/components/pages/TeamChoosePage.jsx
+++ src/components/pages/TeamChoosePage.jsx
@@ -10,21 +10,15 @@ function TeamChoosePage() {
 
   return (
     <>
-      <div
-        style={{ width: "400px", height: "100px", border: "1px solid yellow" }}
-      >
-        <div style={{ width: "100%", height: "100%", border: "1px solid red" }}>
-          Выбери свою команду
-        </div>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-around",
-            border: "2px solid white",
-          }}
-        >
-          <TeamBtn name="Команда 1" onClick={handleShowCard} />
-          <TeamBtn name="Команда 2" />
+      <div className="divMain">
+        <div className="divAllContent">
+          <div style={{ width: "90%", height: "20%", border: "1px solid yellow" }}>
+            <div style={{ width: "100%", height: "100%", border: "1px solid red", fontFamily: "'BabyPop', sans-serif", fontWeight: "500", fontSize: "50px", textAlign: "center" }}>Выбери свою команду</div>
+            <div style={{ display: "flex", justifyContent: "space-around", border: "2px solid white" }}>
+              <TeamBtn name="Команда 1" onClick={handleShowCard} />
+              <TeamBtn name="Команда 2" />
+            </div>
+          </div>
         </div>
       </div>
     </>
